e56264e476dc5dd9b209587bc18b5cdc6d5ff7ab
minhhtt2 <minhhtt2@fpt.edu.vn>
2025-09-17 21:32:43 +0700

    Chuan bi bai mâu sinh vien

---
+++ Lab1/src/main/java/com/mycompany/lab1/bai4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab1;
+
+/**
+ *
+ * @author ICT
+ */
+public class bai4 {
+
+    public static void main(String[] args) {
+          // sinh viên code ở đây
+    }
+}
